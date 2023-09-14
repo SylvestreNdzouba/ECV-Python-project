59f31e6d0a9868e3cd739952cf07b44e1c72f3c2
Gladrat <3368584+Gladrat@users.noreply.github.com>
2023-09-14 10:19:55 +0200

    update

---
+++ cours/01.bases.py
@@ -0,0 +1,50 @@
+print("Hello world !")
+
+# Ceci est un commentaire
+
+mon_entier = 42
+pi = 3.14
+un_texte = "This is a text"
+un_texte2 = "This is a text"
+
+x = 42
+x = x + 1
+x = x - 3
+x = x / 10
+x = x * 10
+x = (x + 1) * 10
+
+# opérateur avancés
+
+y = 3.14
+y = y ** 2
+y = 20 // 6
+y = 20 % 6
+
+print(234 // 13)
+
+# --- MANIPULATIONS DE TEXTE ---
+
+chaine = "Python "
+chaine = chaine + "3.10"
+
+chaine = chaine * 3
+
+print(chaine)
+
+# Types et conversions
+
+chaine = "Python "
+chaine = chaine + str(3.10)
+
+int
+str
+float
+bool
+
+nb_error = "3.10"
+nb = float(nb_error)
+
+print(type(nb))
+
+print(type(nb) == float)
