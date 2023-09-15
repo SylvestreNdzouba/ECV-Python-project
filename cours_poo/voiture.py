a85930e8119f254029327a01c24495f708f8aa1d
Gladrat <3368584+Gladrat@users.noreply.github.com>
2023-09-15 11:34:41 +0200

    update

--- cours_poo/voiture.py
+++ cours_poo/voiture.py
@@ -4,27 +4,41 @@ class Voiture:
     # attributs (variables d'une classe)
 
     # constructeur
-    def __init__(self, couleur = "blanc"):
+    def __init__(self, couleur = "blanc", vitesse_bridage = 250):
         self.nb_roues = 4
-        self.vitesse_max = 250
+        if (vitesse_bridage > 250):
+            vitesse_bridage = 250
+        self.vitesse_max = vitesse_bridage
+        self.vitesse_courante = 0
         self.couleur = couleur
+        self.klaxonner()
         
     def __str__(self):
         return f"Une voiture {self.couleur} pouvant rouler à {self.vitesse_max}km/h"
     
     def klaxonner(self):
         print("Tut tut")
-        
-    # méthodes (fonctions de la classe)
+    
+    def rouler(self, vitesse_cible):
+        if (vitesse_cible <= self.vitesse_max):
+            self.vitesse_courante = vitesse_cible
+        else:
+            self.vitesse_courante = self.vitesse_max
+            
+    # méthode "brider" qui bride la vitesse max de la voiture
+    # la vitesse de bridage ne peut jamais dépasser la vitesse max actuelle
+        # utiliser cette méthode dans le constructeur
 
-ma_voiture = Voiture("rouge")
+ma_voiture = Voiture("rouge", 500)
 print(ma_voiture)
-ma_voiture.klaxonner()
+# ma_voiture.klaxonner()
 
-ta_voiture = Voiture("bleue")
+ta_voiture = Voiture("bleue", 220)
 print(ta_voiture)
-ta_voiture.klaxonner()
+# ta_voiture.klaxonner()
 
 une_voiture = Voiture()
 print(une_voiture)
-une_voiture.klaxonner()
+# une_voiture.klaxonner()
+
+########
